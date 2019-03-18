389e35c548da2387fea098ca5e19be5be48608a1
Benoît Briastre <benoit.briastre@gmail.com>
2019-03-18 12:09:38 +0100

    oups modifs

--- POOEncaps.js
+++ POOEncaps.js
@@ -2,21 +2,19 @@ const assert = require('assert');
 
 // ... Ton code ici ...
 class BankCustomer {
-	constructor(name, code){
-		this.name = name;
-		this.code = code;
-	}
-	getName(){
-		return`${this.name}`
-	}
-	verifyPinInput(){
-		if(this.code > 0){
-			return true;
-		} else {
-			return false;
+	constructor(name_, code_){
+		let name = name_;
+		let code = code_;
+		this.getName = () => name
+		this.verifyPinInput = (verif_) => {
+			if(verif_ === code){
+				return true;
+				} else {
+					return false;
+				}
+			}
 		}
 	}
-}
 
 
 // Tests
@@ -25,3 +23,6 @@ assert.equal(typeof customer.getName, 'function');
 assert.equal(typeof customer.verifyPinInput, 'function');
 assert.equal(customer.getName(), 'John Doe');
 assert.ok(customer.verifyPinInput('3579'));
+
+console.log(customer.getName())
+console.log(customer.verifyPinInput('3579'))
